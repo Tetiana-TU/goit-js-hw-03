392c64c7e50d882b575fd7a69563ee8d6de4070f
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2024-10-28 14:27:51 +0200

    add task 1,2,3

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,12 @@
 function getElementWidth(content, padding, border) {
-  return `${parseFloat(content)}+${parseFloat(padding) * 2}+${
-    parseFloat(border) * 2
-  }`;
+  
+   console.log((Number.parseFloat(content)) + ((Number.parseFloat(padding))*2) + ((Number.parseFloat(border))*2));
 }
 
 getElementWidth('50px', '8px', '4px');
 getElementWidth('60px', '12px', '8.5px');
 getElementWidth('200px', '0px', '0px');
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
